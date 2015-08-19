072c9ae195a90178bea1acdf75b27201ee31eda0
ufownl <ufownl@gmail.com>
2015-08-19 10:18:07 +0800

    增加decorator_not节点

---
+++ decorator_not_sample.cpp
@@ -0,0 +1,33 @@
+#include "sample_util.hpp"
+#include "ranger/bhvr_tree/decorator_not_node.hpp"
+
+int main() {
+	using node_ptr = std::unique_ptr<ranger::bhvr_tree::abstract_node<void>>;
+
+	{
+		node_ptr root(new ranger::bhvr_tree::decorator_not_node<void>);
+		root->attach_child(node_ptr(new true_node));
+
+		ranger::bhvr_tree::agent_proxy<void> ap;
+		root->exec(ap, [] (bool result) {
+			std::cout << result << std::endl;
+		});
+
+		caf::await_all_actors_done();
+	}
+
+	{
+		node_ptr root(new ranger::bhvr_tree::decorator_not_node<void>);
+		root->attach_child(node_ptr(new false_node));
+
+		ranger::bhvr_tree::agent_proxy<void> ap;
+		root->exec(ap, [] (bool result) {
+			std::cout << result << std::endl;
+		});
+
+		caf::await_all_actors_done();
+	}
+
+	caf::shutdown();
+	return 0;
+}
